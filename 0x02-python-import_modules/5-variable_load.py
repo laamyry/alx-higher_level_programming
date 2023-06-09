ff583600f9165281c99c1617e92adefb890483fa
laamyry <slamiri10@gmail.com>
2023-06-09 02:03:07 +0100

    laamyry py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-import hidden_4
 
 if __name__ == "__main__":
-    for item in dir(hidden_4):
-        if not item.startswith("__"):
-            print(item)
+    from variable_load_5 import a
+print(a)

c70cfc285247b209907c8cc89aa564da5c2e96f4
laamyry <slamiri10@gmail.com>
2023-06-09 01:30:40 +0100

    laamyry py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == '__main__':
+
+    import sys
+
+    args = sys.argv[1:]
+    num_args = len(args)
+
+    if num_args == 0:
+        print(".", end="\n")
+    else:
+        print( str(num_args) + " argument:", end="\n")
+        for i, arg in enumerate(args, start=1):
+            print("{}: {}".format(i, arg))

34413c1f1cb95df22233fc608c3b24a62b4c9016
laamyry <slamiri10@gmail.com>
2023-06-13 00:43:58 +0100

    laamyry py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,7 +3,7 @@
 def no_c(my_string):
 
     new = ""
-    for m in range (len(my_string)):
+    for m in range(len(my_string)):
         if my_string[m] not in ('c', 'C'):
             new += my_string[m]
     return new

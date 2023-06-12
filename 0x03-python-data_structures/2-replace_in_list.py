9819201d3937fdb4d98ca6f8ae500166ea2a31aa
laamyry <slamiri10@gmail.com>
2023-06-12 02:20:31 +0100

    laamyry py

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    
+    if idx < 0:
+        return my_list[idx]
+    elif idx > len(my_list) - 1:
+        return my_list[idx]
+    else:
+        my_list[idx] = element
+        return my_list
+    

a682526214442cab2979e082cb38dd5689945e21
laamyry <slamiri10@gmail.com>
2023-06-12 02:04:50 +0100

    laamyry py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,9 +2,9 @@
 
 def element_at(my_list, idx):
 
-	if idx < 0:
-		return 'None'
-	elif idx > len(my_list):
-		return 'None'
-	else:
-		return my_list[idx]
+    if idx < 0:
+        return 'None'
+    elif idx > len(my_list) - 1:
+        return 'None'
+    else:
+        return my_list[idx]

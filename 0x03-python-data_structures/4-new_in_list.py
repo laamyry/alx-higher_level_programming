9d1a22d4c01dd97593cceec98cc4d441432124f6
laamyry <slamiri10@gmail.com>
2023-06-12 20:48:08 +0100

    laamyry py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+
+    copy = my_list.copy()
+    if idx < 0:
+        return copy
+    elif idx > len(my_list) - 1:
+        return copy
+    else:
+        copy[idx] = element
+        return copy

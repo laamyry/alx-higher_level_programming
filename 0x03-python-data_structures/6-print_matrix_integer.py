73d4f23c0121584fa6a07c4eb469852c34fccf04
laamyry <slamiri10@gmail.com>
2023-06-13 00:48:52 +0100

    laamyry py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
 
     for m in range(len(matrix)):
         for n in range(len(matrix[m])):
-            if n not in 0:
+            if n != 0:
                 print(" ", end='')
             print("{:d}".format(matrix[m][n]), end='')
         print()

c23dedefb961e53f81e025b0cd7efc0d85c9a21c
laamyry <slamiri10@gmail.com>
2023-06-15 00:23:51 +0100

    laamyry py

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -4,6 +4,6 @@ def print_sorted_dictionary(a_dictionary):
 
     ls_ord = list(a_dictionary.keys())
     ls_ord.sort()
-    
+
     for m in ls_ord:
         print("{}: {}".format(m, a_dictionary.get(m)))

1efd179e66c1eb023af0d9875f537ecb7f0389d3
laamyry <slamiri10@gmail.com>
2023-06-28 01:52:10 +0100

    laamyry py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
 
-"""defines a square"""
 class Square:
-    """Represents a square with a given size."""
-
-    def __init__(self, size):
-        """Initializes a new Square object.
-
-        Args:
-            size (int): The size of the new square.
-        """
-        self._size = size
+    """This class defines a square."""
+    pass

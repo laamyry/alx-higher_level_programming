93087dd3a89d5975444843756691d22d874785ab
laamyry <slamiri10@gmail.com>
2023-07-06 16:06:45 +0100

    laamyry py

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,65 +1,65 @@
 #!/usr/bin/python3
-"""Define classes for a singly-linked list."""
+"""defines a node of a singly linked list by:"""
 
 
 class Node:
-
-    def __init__(self, da_ta, n_node=None):
-
-        self.da_ta = da_ta
-        self.n_node = n_node
+    '''Node class'''
+    def __init__(self, data, next_node=None):
+        '''init def'''
+        self.data = data
+        self.next_node = next_node
 
     @property
-    def da_ta(self):
-
+    def data(self):
+        '''data def'''
         return (self.__data)
 
-    @da_ta.setter
-    def da_ta(self, value):
+    @data.setter
+    def data(self, value):
         if not isinstance(value, int):
-            raise TypeError("da_ta must be an integer")
+            raise TypeError("data must be an integer")
         self.__data = value
 
     @property
-    def n_node(self):
+    def next_node(self):
 
         return (self.__next_node)
 
-    @n_node.setter
-    def n_node(self, value):
+    @next_node.setter
+    def next_node(self, value):
         if not isinstance(value, Node) and value is not None:
-            raise TypeError("n_node must be a Node object")
+            raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
 
 class SinglyLinkedList:
-
+    '''Node SinglyLinkedList'''
     def __init__(self):
 
         self.__head = None
 
     def sorted_insert(self, value):
 
-        new_node = Node(value)
+        n_node = Node(value)
         if self.__head is None:
-            new_node.n_node = None
-            self.__head = new_node
-        elif self.__head.da_ta > value:
-            new_node.n_node = self.__head
-            self.__head = new_node
+            n_node.next_node = None
+            self.__head = n_node
+        elif self.__head.data > value:
+            n_node.next_node = self.__head
+            self.__head = n_node
         else:
             temp = self.__head
-            while (temp.n_node is not None and
-                    temp.n_node.da_ta < value):
-                temp = temp.n_node
-            new_node.n_node = temp.n_node
-            temp.n_node = new_node
+            while (temp.next_node is not None and
+                    temp.next_node.data < value):
+                temp = temp.next_node
+            n_node.next_node = temp.next_node
+            temp.next_node = n_node
 
     def __str__(self):
-
+        '''str def'''
         values = []
         temp = self.__head
         while temp is not None:
-            values.append(str(temp.da_ta))
-            temp = temp.n_node
+            values.append(str(temp.data))
+            temp = temp.next_node
         return ('\n'.join(values))

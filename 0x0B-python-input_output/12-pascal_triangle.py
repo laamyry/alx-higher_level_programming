fe3e3fc2ad37a8784e6ac091967749f49fed511d
laamyry <slamiri10@gmail.com>
2023-07-12 01:49:14 +0100

    laamyry py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" returns a list of lists of integers representing the Pascal’s triangle of n:"""
+""" returns a list of lists of integers representing
+the Pascal’s triangle of n:"""
 
 
 def pascal_triangle(n):

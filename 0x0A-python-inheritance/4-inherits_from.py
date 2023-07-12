03971e6bba1ea62c2b82e1e038988a0fb321e352
laamyry <slamiri10@gmail.com>
2023-07-13 00:58:53 +0100

    laamyry py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-# 6-from_json_string.py
 """function that returns True if the object is an instance of a class
 that inherited (directly or indirectly) from the specified class"""
 
 
 def inherits_from(obj, a_class):
-    """function that returns True if the object is an instance of a class
-    that inherited (directly or indirectly) from the specified class"""
-    return(issubclass(type(obj), a_class) and type(obj) != a_class)
+    """def inherits_from"""
+    return (issubclass(type(obj), a_class) and type(obj) != a_class)

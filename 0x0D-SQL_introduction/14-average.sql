122449d514d1183bc9e62a9c84e3d408c5e3a142
laamyry <slamiri10@gmail.com>
2023-08-16 01:00:09 +0100

    laamyry py

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,2 +1,2 @@
 -- computes the score average of all records in the table second_table of the database hbtn_0c_0 in your MySQL server.
-UPDATE second_table SET score=10 WHERE name="Bob";
+SELECT AVG(`score`) AS `average` FROM `second_table`;

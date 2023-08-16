5b6ef46fa977c7ed8ac917c5bd16d006fd5ce85c
laamyry <slamiri10@gmail.com>
2023-08-16 01:02:58 +0100

    laamyry py

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
-SELECT score, COUNT(*) as number FROM second_table GROUP BY score ORDER BY number DESC;
+SELECT `score`, `name` FROM `second_table` WHERE `name` != "" ORDER BY `score` DESC

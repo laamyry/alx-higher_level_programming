c40b629ef63fb1c605a3d41ac0f1b33d23d6aaba
laamyry <slamiri10@gmail.com>
2023-08-18 03:19:33 +0100

    laamyry py

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,5 +1,4 @@
--- creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server.
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_usa';
+-- creates the database hbtn_0d_usa and the table citiesCREATE DATABASE IF NOT EXISTS 'hbtn_0d_usa';
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT NOT NULL UNIQUE PRIMARY KEY
     AUTO_INCREMENT, state_id INT NOT NULL, FOREIGN KEY(state_id)
     REFERENCES hbtn_0d_usa.states(id), name VARCHAR(256) NOT NULL);

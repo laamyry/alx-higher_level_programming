8d18b9dffb1a9dc394d566978091f74d83ba02f0
laamyry <slamiri10@gmail.com>
2023-08-22 17:34:37 +0100

    laamyry py

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
--- Lists all privileges granted to two users
+-- lists all privileges of the MySQL users
 -- localhost server
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';

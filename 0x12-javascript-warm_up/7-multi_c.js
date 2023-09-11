c3a9f9a6cab35b483c27fa74c7a44b8b7407a4aa
laamyry <slamiri10@gmail.com>
2023-09-11 23:24:53 +0100

    Laamyry Js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+let m = 0;
+const arg = parseInt(process.argv[2]);
+if (!isNaN(arg) && arg > 0) {
+  while (m < arg) {
+    console.log("C is fun");
+    m++;
+  }
+} else {
+  console.log("Missing number of occurrences");
+}

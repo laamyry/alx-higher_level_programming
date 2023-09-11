a65c1a14d1054fb00e579958bcde5a9d220da660
laamyry <slamiri10@gmail.com>
2023-09-11 22:20:17 +0100

    laamyry py

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
-arg = process.argv;
-if (arg.length == 2) {
-    console.log('No argument');
-}
-else if (arg.length == 3) {
-    console.log('Argument found');
-}
-else {
-    console.log('Arguments found');
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }

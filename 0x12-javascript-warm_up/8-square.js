4811541bcbe6db28f7f1fd9f1ce9e2bdafd9807f
laamyry <slamiri10@gmail.com>
2023-09-12 02:06:33 +0100

    Laamyry Js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,7 +2,7 @@
 const args = process.argv.slice(2);
 const size = parseInt(args[0]);
 if (isNaN(size)) {
-  console.log("Missing size");
+  console.log('Missing size');
 } else {
   for (let m = 0; m < size; m++) {
     let row = '';

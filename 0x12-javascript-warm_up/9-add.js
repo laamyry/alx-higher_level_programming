0cc4cbbe2f9f3ebae677a7eb9ace0b9efeb55d3f
laamyry <slamiri10@gmail.com>
2023-09-12 02:14:47 +0100

    Laamyry Js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,9 +2,9 @@
 const args = process.argv.slice(2);
 const number1 = parseInt(args[0]);
 const number2 = parseInt(args[1]);
-if (isNaN(number1) || isNaN(num2)) {
+if (isNaN(number1) || isNaN(number2)) {
   console.log("NaN");
 } else {
-  const result = number1 + num2;
+  const result = number1 + number2;
   console.log(result);
 }

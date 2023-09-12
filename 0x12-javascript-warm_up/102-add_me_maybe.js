9dd9acac3252a673b086ebcbc85cee2211d0a8ad
laamyry <slamiri10@gmail.com>
2023-09-12 03:21:08 +0100

    Laamyry Js

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 function addMeMaybe (number, theFunction) {
-      const result = number + 1;
-      theFunction(result);
-  }
-  module.exports = { addMeMaybe };
-  
+  const result = number + 1;
+  theFunction(result);
+}
+module.exports = { addMeMaybe };

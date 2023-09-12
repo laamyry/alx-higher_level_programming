7e1c0e2b1add60d16211c798d3cf53f4496c80ff
laamyry <slamiri10@gmail.com>
2023-09-12 22:53:09 +0100

    laamyry py

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+const args = process.argv.slice(2).map(Number);
+if (args.length > 1) {
+  console.log(Math.max(...args.filter(num => num !== Math.max(...args))));
+} else {
+  console.log(0);
+}

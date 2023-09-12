5a27e857214778048cbee1f42e512e952e009fa4
laamyry <slamiri10@gmail.com>
2023-09-12 02:27:01 +0100

    Laamyry Js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
-function add(a, b) {
-    const number1 = parseInt(a);
-    const number2 = parseInt(b);
-  
-    if (isNaN(number1) || isNaN(number2)) {
-      console.log('NaN');
-    } else {
-      console.log(number1 + number2);
-    }
+function add (a, b) {
+  const number1 = parseInt(a);
+  const number2 = parseInt(b);
+
+  if (isNaN(number1) || isNaN(number2)) {
+    console.log('NaN');
+  } else {
+    console.log(number1 + number2);
   }
-  
-  const arg1 = process.argv[2];
-  const arg2 = process.argv[3];
-  
-  add(arg1, arg2);
+}
+
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+add(arg1, arg2);

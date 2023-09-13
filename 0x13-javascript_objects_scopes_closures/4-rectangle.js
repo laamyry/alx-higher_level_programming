4598798a756b918818d2a8458c837193b91ecd7a
laamyry <slamiri10@gmail.com>
2023-09-13 03:09:55 +0100

    Laamyry Js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,1 +1,29 @@
 #!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 || h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let m = 0;
+    while (m < this.height) {
+      console.log('X'.repeat(this.width));
+      m++;
+    }
+  }
+
+  rotate () {
+    this.width = this.height;
+    this.height = this.width;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;

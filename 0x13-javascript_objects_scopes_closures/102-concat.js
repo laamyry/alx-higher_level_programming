fb00140c7a4352d0882fe165b6698b33fcc47f69
laamyry <slamiri10@gmail.com>
2023-09-13 03:58:32 +0100

    Laamyry Js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const fs = require('fs');
-const [src1, src2] = process.argv.slice(2);
-fs.writeFileSync(fs.readFileSync(src1) + fs.readFileSync(src2));
+const [src1, src2, dest] = process.argv.slice(2);
+fs.writeFileSync(dest, fs.readFileSync(src1) + fs.readFileSync(src2));

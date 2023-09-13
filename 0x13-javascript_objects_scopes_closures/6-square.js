63763f7272fa51acb360b3c49150eaefd7a742f3
laamyry <slamiri10@gmail.com>
2023-09-13 03:36:53 +0100

    Laamyry Js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+const SquareBase = require('./5-square');
+
+class Square extends SquareBase {
+  charPrint(c = 'X') {
+    let m = 0;
+    while ( m < this.height) {
+      console.log(c.repeat(this.width));
+      m++;
+    }
+  }
+}
+module.exports = Square;

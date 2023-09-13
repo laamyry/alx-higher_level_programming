c61a991c1378f944607489adf356161bc2010d4e
laamyry <slamiri10@gmail.com>
2023-09-13 03:50:06 +0100

    Laamyry Js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+const dict = require('./101-data').dict;
+const newDict = {};
+
+for (const key in dict) {
+  const value = dict[key];
+  if (!newDict[value]) {
+    newDict[value] = [key];
+  }
+}
+
+console.log(newDict);

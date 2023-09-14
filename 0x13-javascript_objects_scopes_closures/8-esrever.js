c641b637cfefd2fcde9c79128f16c90fb7af9a7e
laamyry <slamiri10@gmail.com>
2023-09-14 01:39:39 +0100

    laamyry py

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-exports.esrever = function(list) {
-    let reversedlist = list;
-    m = list.length - 1;
-    while (m => 0) {
-        reversedlist.push(list[m])
-    }
-    return reversedlist;
-}
+exports.esrever = function (list) {
+  const reversedlist = list;
+  let m = list.length - 1;
+  while (m => 0) {
+    reversedlist.push(list[m]);
+    m--;
+  }
+  return reversedlist;
+};

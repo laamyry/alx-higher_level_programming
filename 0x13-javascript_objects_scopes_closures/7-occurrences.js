681fd457ac4e49c08eeb8c10c8f493eb1fa1d1b2
laamyry <slamiri10@gmail.com>
2023-09-14 01:29:19 +0100

    laamyry py

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  let m = 0;
+  while (m < list.length) {
+    if (list[m] === searchElement) {
+      count++;
+    }
+    m++;
+  }
+  return count;
+};

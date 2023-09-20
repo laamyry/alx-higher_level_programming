24246a1c8d55674b507fc8aaa7f72df76a76ef67
laamyry <slamiri10@gmail.com>
2023-09-21 00:00:05 +0100

    Laamyry Py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+''' lists all states with a name starting with N (upper N)
+    from the database hbtn_0e_0_usa'''
+import MySQLdb
+import sys
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost",
+                         user=sys.argv[1],
+                         port=3306,
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+cur = db.cursor()
+
+cur.execute("SELECT * FROM stats WHERE name like 'N%' ORDER BY id ASC")
+[print(row) for row in cur.fetchall()]
+
+cur.close()

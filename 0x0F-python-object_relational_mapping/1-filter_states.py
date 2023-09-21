eddb4c4776c422ad6525f58049f84900398531f7
laamyry <slamiri10@gmail.com>
2023-09-21 01:32:35 +0100

    Laamyry Py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,10 +7,10 @@ from sys import argv
 
 if __name__ == '__main__':
     db = database.connect(host="localhost",
-                         port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3])
+                          user=argv[1],
+                          passwd=argv[2],
+                          db=argv[3],
+                          port=3306)
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' \
                 ORDER BY states.id ASC")

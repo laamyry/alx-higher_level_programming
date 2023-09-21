ae7755aa2eff94f55dd13973e7b047070b562ac0
laamyry <slamiri10@gmail.com>
2023-09-21 01:44:48 +0100

    Laamyry Py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""lists all states with a name starting with N
+(upper N) from the database hbtn_0e_0_usa"""
+
+import MySQLdb as database
+from sys import argv
+
+if __name__ == '__main__':
+    db = database.connect(host="localhost",
+                          user=argv[1],
+                          passwd=argv[2],
+                          db=argv[3],
+                          port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id", (argv[4],))
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()

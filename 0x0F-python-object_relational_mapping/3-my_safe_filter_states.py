35c34f91a000028784af3fd72957d59f362f276d
laamyry <slamiri10@gmail.com>
2023-09-23 00:49:36 +0100

    laamyry py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -11,8 +11,7 @@ if __name__ == '__main__':
                           db=argv[3],
                           port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states \
-                WHERE name = '{:s}' ORDER BY id ASC;".format(argv[4]))
+    cur.execute("SELECT * FROM states WHERE name = %s;", (argv[4]))
 
     rows = cur.fetchall()
 

0a754f18fd7f2208772bfe1b538ad2ba883465d2
laamyry <slamiri10@gmail.com>
2023-09-23 01:41:46 +0100

    Laamyry Py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -11,11 +11,11 @@ if __name__ == '__main__':
                           db=argv[3],
                           port=3306)
     cur = db.cursor()
-    cur.execute("""SELECT cities.name
+    cur.execute("""SELECT cities.name 
                 FROM cities JOIN states
                 ON cities.state_id = states.id
-                WHERE states.name = %s
-                ORDER BY cities.id ASC""", format(argv[4]))
+                WHERE states.name=%s
+                ORDER BY cities.id""", (argv[4],))
 
     rows = cur.fetchall()
 

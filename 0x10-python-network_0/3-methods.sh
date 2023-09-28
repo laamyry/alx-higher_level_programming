fdca12ea995f9a7eb64873cb4c982d6a67901a6b
laamyry <slamiri10@gmail.com>
2023-09-29 00:34:02 +0100

    0x10-python-network_0

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a GET request to the URL, and displays the body of the response.
+# takes in a URL and displays all HTTP methods the server will accept.
+curl -Si "$1" | grep Allow 

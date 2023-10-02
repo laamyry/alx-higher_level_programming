37e015fb49094deccc3dadda22936df071b469f4
laamyry <slamiri10@gmail.com>
2023-10-02 22:02:27 +0100

    laamyry py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+'''takes in a URL and an email address, sends a POST request to
+the passed URL with the email as a parameter, and finally displays
+the body of the response.'''
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    mail = argv[2]
+    data = {'email': mail}
+    
+    try:
+        resp = requests.post(url, data)
+        print(resp.text)
+
+    except Exception as exc:
+        print(exc)

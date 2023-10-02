c082ca3f3f9a43fa4caa86f53b5f3f33b1e6b930
laamyry <slamiri10@gmail.com>
2023-10-02 13:15:52 +0100

    laamyry py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
         with urllib.request.urlopen(url) as resp:
             content = resp.read()
             utf8_d = content.decode('utf-8')
+            print("Body response:")
             print("\t- type: {}".format(type(content)))
             print("\t- content: {}".format(content))
             print("\t- utf8 content: {}".format(utf8_d))

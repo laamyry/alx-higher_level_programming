3bb536b6f1c1d6574cef2eb69777e136734b6e46
laamyry <slamiri10@gmail.com>
2023-10-02 13:34:05 +0100

    laamyry py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,10 +2,9 @@
 '''takes in a URL, sends a request to the URL and displays the value of
 the X-Request-Id variable found in the header of the response.'''
 import urllib.request as request
-import sys
-
+from sys import argv
 if __name__ == "__main__":
-    url = sys.argv[0]
+    url = argv[0]
 
     try:
         with request.urlopen(url) as resp:

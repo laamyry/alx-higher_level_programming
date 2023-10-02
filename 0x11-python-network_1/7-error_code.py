71abe6bd31bf0e392680e34430091433504d7af9
laamyry <slamiri10@gmail.com>
2023-10-02 22:30:00 +0100

    laamyry py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+'''takes in a URL, sends a request to the URL and displays the body of the response.'''
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+
+    try:
+        resp = requests.get(url)
+        if resp.status_code >= 400:
+            print("Error code: ".format(resp.status_code))
+        else:
+            print(resp.text)
+
+    except Exception as exc:
+        print(exc)

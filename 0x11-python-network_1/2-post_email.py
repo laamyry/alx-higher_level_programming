f77078a6ab051fa715b3184803c08fba9513d241
laamyry <slamiri10@gmail.com>
2023-10-02 13:51:04 +0100

    laamyry py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-'''takes in a URL and an email, sends a POST request to the passed URL with
-the email as a parameter, and displays the body of the response (decoded in utf-8)'''
+'''takes in a URL and an email, sends a POST request
+to the passed URL with the email as a parameter, and
+displays the body of the response (decoded in utf-8)'''
 import urllib.request as request
 import urllib.parse as parse
 from sys import argv

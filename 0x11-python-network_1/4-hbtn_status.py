1ba5d328aa24d1b026a8e7e7ba2942d1a8c24e51
laamyry <slamiri10@gmail.com>
2023-10-02 21:24:47 +0100

    laamyry py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+'''fetches https://alx-intranet.hbtn.io/status'''
+import requests
+
+if __name__ == "__main__":
+    url = 'https://intranet.hbtn.io/status'
+
+    try:
+        resp = requests.get(url)
+        print("Body response:")
+        print("\t- type: {}".format(type(resp)))
+        print("\t- content: {}".format(resp))
+        print("\t- utf8 content: {}".format(resp))
+
+    except Exception as exc:
+        print(exc)

e6ecab123a05b2b2cccc9f72773ff4651ec9c462
laamyry <slamiri10@gmail.com>
2023-10-02 23:07:09 +0100

    laamyry py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -3,4 +3,14 @@
 the GitHub API to display your id'''
 import requests
 from sys import argv
-from requests.auth import HTTPBasicAuth
+from requests.auth import HTTPBasicAuth as hba
+
+if __name__ == "__main__":
+    url = "https://api.github.com/user"
+
+    try:
+        resp = requests.get(url, auth=hba(argv[1], argv[2]))
+        print(resp.json()['id'])
+
+    except Exception as exc:
+        print("None")

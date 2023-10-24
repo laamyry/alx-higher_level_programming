b12196d60b2c99e570f51b1cb7690124c4e05d4c
laamyry <slamiri10@gmail.com>
2023-10-25 00:30:43 +0100

    laamyry py

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+const request = require('request');
+
+const url = process.argv[1];
+
+request(url, (er, response) => {
+  if (er) {
+    console.error(er);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
+});

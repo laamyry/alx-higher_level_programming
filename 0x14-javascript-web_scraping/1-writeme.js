7f79a71b838350ca9af882ce5a9b6594fd7dbbd6
laamyry <slamiri10@gmail.com>
2023-10-25 00:19:46 +0100

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const fs = require('fs');
+
+const path = process.argv[2];
+const data = process.argv[3];
+
+fs.writeFile(path, data, 'utf-8', (er) => {
+  if (er) {
+    console.error(er);
+  }
+});

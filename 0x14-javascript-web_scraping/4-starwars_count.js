f29e77cd9645776c8b1d1dfb971b931a3cf16e64
laamyry <slamiri10@gmail.com>
2023-10-25 23:09:59 +0100

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (er, resp, body) {
+  if (!er) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
+  }
+});

729560cf221adf9e53ee9a436406da3314199d1b
laamyry <slamiri10@gmail.com>
2023-10-25 23:24:42 +0100

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+const request = require('request');
+
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (er, resp, body) {
+  if (!er) {
+    const characters = JSON.parse(body).characters;
+    characters.forEach((character) => {
+      request(character, function (er, resp, body) {
+        if (!er) {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    });
+  }
+});
--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,24 @@
 #!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+request(url, function (er, resp, body) {
+  if (er) {
+    console.log(er);
+  } else if (resp.statusCode === 200) {
+    const completed = {};
+    const tsks = JSON.parse(body);
+    for (const m in tsks) {
+      const tsk = tsks[m];
+      if (tsk.completed === true) {
+        if (completed[tsk.userId] === undefined) {
+          completed[tsk.userId] = 1;
+        } else {
+          completed[tsk.userId]++;
+        }
+      }
+    }
+    console.log(completed);
+  }
+});

44a7ac7e602527a569d7d3a0350c396bd0eee8d3
laamyry <slamiri10@gmail.com>
2023-10-25 01:27:59 +0100

    laamyry py

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const request = require('request');
+
+const movieid = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieid}`;
+
+request.get(url, (er, resp, body) => {
+  if (er) {
+    console.log(er);
+  } else {
+    const data = JSON.parse(body);
+    console.log(data.title);
+  }
+});

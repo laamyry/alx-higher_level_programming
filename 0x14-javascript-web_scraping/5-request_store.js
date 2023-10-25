964a84b5d7a307f898458576f1eacb22fe58548c
laamyry <slamiri10@gmail.com>
2023-10-25 23:16:56 +0100

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+const request = require('request');
+
+const fs = require('fs');
+const url = process.argv[2];
+const path = process.argv[3];
+request(url).pipe(fs.createWriteStream(path));

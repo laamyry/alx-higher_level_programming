df0eeae54365cc360e14a64b3abd7bfc50222efb
laamyry <slamiri10@gmail.com>
2023-11-09 01:33:03 +0100

    0x0C-python-almost_a_circl

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,9 +3,11 @@
 
 
 class Base:
+    '''class Base:'''
     __nb_objects = 0
 
     def __init__(self, id=None):
+        '''__init__'''
         if id is not None:
             self.id = id
         else:

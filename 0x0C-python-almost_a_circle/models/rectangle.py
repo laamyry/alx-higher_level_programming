62354ea526322e982b6f2e581bb8e410eac120c5
laamyry <slamiri10@gmail.com>
2023-11-09 01:54:15 +0100

    0x0C-python-almost_a_circl

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,39 +7,39 @@ class Rectangle(Base):
     '''First Rectangle'''
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
-
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+    @property
     def get_width(self):
         '''get_width'''
-        return self.width
-
+        return self.__width
+    @__width.setter
     def set_width(self, value):
         '''set_width'''
-        self.width = value
-
+        self.__width = value
+    @property
     def get_height(self):
         '''get_height'''
-        return self.height
-
+        return self.__height
+    @__height.setter
     def set_height(self, value):
         '''set_height'''
-        self.height = value
-
+        self.__height = value
+    @property
     def get_x(self):
         '''get_x'''
-        return self.x
-
+        return self.__x
+    @__x.setter
     def set_x(self, value):
         '''set_x'''''
-        self.x = value
-
+        self.__x = value
+    @property
     def get_y(self):
         '''get_y'''
-        return self.y
-
+        return self.__y
+    @__y.setter
     def set_y(self, value):
         '''set_y'''
-        self.y = value
+        self.__y = value

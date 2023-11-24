0825aff8259c5293fb12527972c38229aa078b20
laamyry <slamiri10@gmail.com>
2023-11-24 02:43:47 +0100

    all

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,13 +1,43 @@
 #!/usr/bin/python3
-'''Square module'''
+'''module doc for square'''
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-
     def __init__(self, size, x=0, y=0, id=None):
-        """Initializes the square"""
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+        return f"[Square] ({self.id}) \
+    {self.x}/{self.y} - {self.width}"
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, vl):
+        self.check_int("width", vl)
+        self.check_positive("width", vl)
+        self.width = vl
+
+    def update(self, *args, **kwargs):
+        if len(args) == 0:
+            id = kwargs["id"] if "id" in kwargs else self.id
+            self.id = id
+            size = kwargs["size"] if "size" in kwargs else self.width
+            x = kwargs["x"] if "x" in kwargs else self.x
+            y = kwargs["y"] if "y" in kwargs else self.y
+        else:
+            if len(args) >= 1:
+                self.id = args[0]
+            size = args[1] if len(args) >= 2 else self.width
+            x = args[2] if len(args) >= 3 else self.x
+            y = args[3] if len(args) >= 4 else self.y
+        self.checks(size, size, x, y)
+        self.width = size
+        self.x = x
+        self.y = y
+
+    def to_dictionary(self):
+        return {'id': self.id, 'x': self.x, 'size': self.size, 'y': self.y}

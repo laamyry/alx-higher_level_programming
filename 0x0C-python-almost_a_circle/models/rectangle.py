e2ae91effe4d765cd4ee7678c85c69033c7f1e9c
laamyry <slamiri10@gmail.com>
2023-11-24 02:33:02 +0100

    .

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,7 +4,6 @@ from models.base import Base
 
 
 class Rectangle(Base):
-    '''First Rectangle'''
     def __init__(self, width, height, x=0, y=0, id=None):
         '''__init__'''
         super().__init__(id)
@@ -15,51 +14,41 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        '''get_width'''
         return self.__width
 
     @width.setter
     def width(self, value):
-        '''set_width'''
         self.validate("width", value, False)
         self.__width = value
 
     @property
     def height(self):
-        '''get_height'''
         return self.__height
 
     @height.setter
     def height(self, value):
-        '''set_height'''
         self.validate("height", value, False)
         self.__height = value
 
     @property
     def x(self):
-        '''get_x'''
         return self.__x
 
     @x.setter
     def x(self, value):
-        '''set_x'''''
         self.validate("x", value)
         self.__x = value
 
     @property
     def y(self):
-        '''get_y'''
-
         return self.__y
 
     @y.setter
     def y(self, value):
-        '''set_y'''
         self.validate("y", value)
         self.__y = value
 
     def validate(self, name, value, fl=True):
-        '''Validate attributes'''
         if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if fl and value < 0:
@@ -68,25 +57,21 @@ class Rectangle(Base):
             raise ValueError(f"{name} must be > 0")
 
     def area(self):
-        ''' adding the public method'''
         ar = self.width * self.height
         return ar
 
     def display(self):
-        '''adding the public method def display(self)'''
         dis = '\n' * self.y + (' ' * self.x + '#' *
                                self.width + '\n') * self.height
         print(dis, end='')
 
     def __str__(self):
-        '''Overriding the __str__ method'''
         return (
             f'[{type(self).__name__}] ({self.id}) '
             f'{self.x}/{self.y} - {self.width}/{self.height}'
         )
 
     def up_display(self, id=None, width=None, height=None, x=None, y=None):
-        '''improving the public method def display(self)'''
         if id is not None:
             self.id = id
         if width is not None:
@@ -99,7 +84,6 @@ class Rectangle(Base):
             self.y = y
 
     def update(self, *args, **kwargs):
-        '''adding the public method def update(self, *args)'''
         if args:
             self.up_display(*args)
         elif kwargs:
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,24 +1,13 @@
 #!/usr/bin/python3
-'''Rectangle module'''
+'''Square module'''
+from models.rectangle import Rectangle
 
 
-class Rectangle:
-    def __init__(self, width, height, x=0, y=0, id=None):
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
-        self.id = id
+class Square(Rectangle):
 
-    def update(self, *args, **kwargs):
-        if args:
-            attrs = ['id', 'width', 'height', 'x', 'y']
-            for m, arg in enumerate(args):
-                setattr(self, attrs[m], arg)
-        else:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initializes the square"""
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        return f"[Rectangle]
-        ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"

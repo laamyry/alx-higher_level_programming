b54690ac5afd01ab757a2d64af4c572c7b195ef3
laamyry <slamiri10@gmail.com>
2023-12-01 00:56:28 +0100

    update rectangle file

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -102,3 +102,10 @@ class Rectangle(Base):
             self.up_display(*args)
         elif kwargs:
             self.up_display(**kwargs)
+
+    def to_dictionary(self):
+        '''doc of square'''
+        return {"id": self.id, "size": self.width, "x": self.x, "y": self.y}
+
+    def __str__(self):
+        return f"[Square] ({self.id}) {self.x}/{self.x} - {self.width}"
